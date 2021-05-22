0f3ed3527357baf7c236fee9cdc55858fd19d7f1
Haruki Honda <yuji515091@gmail.com>
2021-05-22 21:26:48 +0900

    Card game design (3)

---
+++ index.java
@@ -0,0 +1,52 @@
+// import java.util.Arrays;
+import java.util.ArrayList;
+
+class Card{
+  public String suit;
+  public String value;
+  public int intValue;
+
+  public Card(String suit, String value, int intValue){
+    this.suit = suit;
+    this.value = value;
+    this.intValue = intValue;
+  }
+
+  public String getCardString(){
+    return this.suit + this.value + "(" + this.intValue + ")";
+  }
+}
+
+class Deck{
+  public ArrayList<Card> deck;
+  
+  public Deck(){
+    this.deck = Deck.generateDeck();
+  }
+
+  public static ArrayList<Card> generateDeck(){
+    ArrayList<Card> newDeck = new ArrayList<>();
+    String[] suits = new String[]{"♣", "♦", "♥", "♠"};
+    String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+    for(int i = 0; i < suits.length; i++){
+      for(int j = 0; j < values.length; j++){
+        newDeck.add(new Card(suits[i], values[j], j+1));
+      }
+    }
+    return newDeck;
+  }
+
+  public void printDeck(){
+    for(int i = 0; i < this.deck.size(); i++){
+      System.out.println(this.deck.get(i).getCardString());
+    }
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+    Deck deck1 = new Deck();
+    deck1.printDeck();
+  }
+}
